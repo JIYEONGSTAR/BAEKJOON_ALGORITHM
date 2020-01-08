d8e8f2ab86ebb975f55a2c234610ddd7dfd220a2
JIYEONGSTAR <0315jiji@daum.net>
2020-01-09 02:40:56 +0900

    20200109

--- Java/BOJ_stringAlgoritm/src/Q11654.java
+++ Java/BOJ_stringAlgoritm/src/Q11654.java
@@ -1,9 +1,16 @@
-
+//아스키 코드
+import java.util.Scanner;
 public class Q11654 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		char x = sc.nextLine().charAt(0);
+		int ascii = (int)x;
+		
+		System.out.println(ascii);
+		
+		sc.close();
 	}
 
 }
