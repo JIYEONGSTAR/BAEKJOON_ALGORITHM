3ac9b3a70815f530ca1fdb151d622d7dda4c9c2c
JIYEONGSTAR <0315jiji@daum.net>
2020-01-09 03:13:33 +0900

    20200105

--- Java/BOJ_forAlgoritm/src/Q10950.java
+++ Java/BOJ_forAlgoritm/src/Q10950.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class Q10950{
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		int a[] = new int[T];
-		int b[] = new int[T];
-		int c[] = new int[T];
-		for(int i=0;i<T;i++) {
+		int t = sc.nextInt();
+		int a[] = new int[t];
+		int b[] = new int[t];
+		int c[] = new int[t];
+		for(int i=0;i<t;i++) {
 			a[i]=sc.nextInt();
 			b[i]=sc.nextInt();
 			c[i]=a[i]+b[i];
 		}
-		for(int i=0;i<T;i++) {
+		for(int i=0;i<t;i++) {
 			System.out.println(c[i]);
 		}
 		sc.close();
