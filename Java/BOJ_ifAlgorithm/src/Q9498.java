4dcf7d84ebcf830d6c3fc2093670e0b9fcc6ee85
JIYEONGSTAR <0315jiji@daum.net>
2020-01-12 06:55:37 +0900

    20201231

--- Java/BOJ_ifAlgorithm/src/Q9498.java
+++ Java/BOJ_ifAlgorithm/src/Q9498.java
@@ -6,21 +6,17 @@ public class Q9498 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc= new Scanner(System.in);
-		int score=sc.nextInt();
-		if(score>=90) {
+		Scanner sc = new Scanner(System.in);
+		int score = sc.nextInt();
+		if (score >= 90) {
 			System.out.println("A");
-		}
-		else if(score>=80) {
+		} else if (score >= 80) {
 			System.out.println("B");
-		}
-		else if(score>=70) {
+		} else if (score >= 70) {
 			System.out.println("C");
-		}
-		else if(score>=60) {
+		} else if (score >= 60) {
 			System.out.println("D");
-		}
-		else
+		} else
 			System.out.println("F");
 		sc.close();
 	}
