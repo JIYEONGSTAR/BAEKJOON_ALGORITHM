3b618212173c660f9446a2fbde00322bf7b6f11e
JIYEONGSTAR <jiyeongstar@gmail.com>
2020-09-24 20:49:51 +0900

    20200924

---
+++ Java/BOJ_forAlgorithm/src/Q2443.java
@@ -0,0 +1,24 @@
+//º° Âï±â - 6
+
+import java.util.Scanner;
+
+public class Q2443 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int j=n;j>=1;j--) {
+			for(int i=0;i<n-j;i++) {
+				System.out.print(" ");
+			}
+			for(int i=1;i<=j*2-1;i++) {
+				System.out.print("*");
+			}
+		
+			System.out.println();
+		}
+		sc.close();
+	}
+
+}
